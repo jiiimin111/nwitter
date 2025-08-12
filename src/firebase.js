74734a009608663d5920e6eaf33ba6e522ae881b
jiiimin111 <gx2dvxbe@sookmyung.ac.kr>
2025-08-13 03:12:16 +0900

    2장

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBn_bUqMTbA2aOqytznxH_zgnntx23U52Q",
+  authDomain: "nwitter-142f6.firebaseapp.com",
+  projectId: "nwitter-142f6",
+  storageBucket: "nwitter-142f6.firebasestorage.app",
+  messagingSenderId: "839987837299",
+  appId: "1:839987837299:web:84d39938ca5a4a1c366d06"
+};
+
+export default firebase.initializeApp(firebaseConfig);
