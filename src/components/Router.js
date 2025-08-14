689cd870122ac8ce75abe70a24526e175af537dc
jiiimin111 <gx2dvxbe@sookmyung.ac.kr>
2025-08-15 02:59:17 +0900

    3장완료

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,14 +4,13 @@ import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
 const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
     return (
         <Router>
             <Switch>
                 {isLoggedIn ? (
                     <Route exact path="/">
                         <Home />
-                        </Route>
                     </Route>
                 ) : (
                     <Route exact path="/">
